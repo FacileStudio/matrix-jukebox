c8718a0e7905af3b87ba2dcfbe8706e33caa2c93
esoteric_programmer <nomail@no.email>
2025-05-07 20:38:53 +0300

    add tokio and decorate main function appropriately

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use tracing::info;
 mod logging;
 #[tracing::instrument(ret, err)]
-fn main() -> eyre::Result<()> {
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> eyre::Result<()> {
     color_eyre::install()?;
     logging::init();
     info!("Hello, world!");
